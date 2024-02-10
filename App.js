feeffd989db41e6176bc72618701b1c4d5b877cf
Your Name <you@example.com>
2024-02-10 19:40:10 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,3 @@
+/* All the code is maintained in their respective html pages */
+/*JQuery is used to maintain the footer and header for all the pages efficiently*/
+/*We have mainly used js for login function in teacherLogin page,student login page and the index page*/
